629c881488ddf253fe6089dad820c8f516dc9c92
IRSTE <syracusebear@gmail.com>
2024-07-17 15:51:58 -0400

    Added Top Bar + Back Navigation

--- app/src/main/java/com/example/baguessr30/BAScreen.kt
+++ app/src/main/java/com/example/baguessr30/BAScreen.kt
@@ -1,7 +1,13 @@
 package com.example.baguessr30
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,11 +17,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.baguessr30.ui.BAViewModel
 import com.example.baguessr30.ui.ChooseApp
@@ -24,27 +32,38 @@ import com.example.baguessr30.ui.StudyApp
 import com.example.baguessr30.ui.SummaryScreen
 import com.example.baguessr30.ui.TitleScreen
 
-enum class BAScreen() {
-    Title,
-    Study,
-    Choose,
-    Game,
-    Summary
+enum class BAScreen(@StringRes val title: Int) {
+    Title(title = R.string.blue_archive_guessing_game),
+    Study(title = R.string.study),
+    Choose(title = R.string.choose_gamemode),
+    Game(title = R.string.guessing_game),
+    Summary(title = R.string.summary)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BAScreenAppBar(
-
+    currentScreen: BAScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-//    TopAppBar(title = { Text(text = "") },
-//        colors = TopAppBarDefaults.mediumTopAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.primaryContainer
-//        )
-//    )
+    CenterAlignedTopAppBar(title = { Text(text = stringResource(currentScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+
+                }
+            }
+        }
+
+
+    )
+
 
 }
-
 
 
 @Composable
@@ -52,8 +71,20 @@ fun BAApp(
     viewModel: BAViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = BAScreen.valueOf(
+        backStackEntry?.destination?.route ?: BAScreen.Title.name
+    )
+
     Scaffold(
-        topBar = { BAScreenAppBar() }
+        topBar = {
+            BAScreenAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
@@ -117,8 +148,6 @@ fun BAApp(
         }
 
     }
-
-
 
 
 }
